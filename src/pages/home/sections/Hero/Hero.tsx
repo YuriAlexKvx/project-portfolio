2b4d53658cc5b3030a8a7b5f891c04e659e974dd
Yuri <yuri.oliveirakvx@gmail.com>
2025-07-03 18:33:55 -0300

    03/07/2025

--- src/pages/home/sections/Hero/Hero.tsx
+++ src/pages/home/sections/Hero/Hero.tsx
@@ -1,29 +1,59 @@
-import { styled } from "@mui/material"
-import Avatar from "../../../../assets/imgs/avatar.jpg"
+import { styled } from "@mui/material/styles";
+import Avatar from "../../../../assets/imgs/avatar.jpg";
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+import { Button, Typography } from "@mui/material";
+import DownloadIcon from '@mui/icons-material/Download';
+import EmailIcon from '@mui/icons-material/Email';
+
+const StyledHero = styled("div")({
+  backgroundColor: "black",
+  height: "100vh",
+});
+
+const StyledAvatar = styled("img")({
+  width: "100%",
+  borderRadius: "50%",
+});
 
 const Hero = () => {
-  
-    const StyledHero = styled("div")(()=> ({
-
-        backgroundColor: "black"
-
-    }))
-
-    const StyledAvatar = styled("img")(()=> ({
-        width: "30%",
-        borderRadius: "50%"
-    }))
-
   return (
-    <>
-
     <StyledHero>
-    Olá, mundo!
-      <StyledAvatar src={Avatar}/>
+      <Container maxWidth="lg">
+        
+      <Grid container spacing={2}>
+        <Grid size={{ xs: 12, md: 4 }}>
+      <StyledAvatar src={Avatar} />
+        </Grid>
+        <Grid size={{ xs: 12, md: 8 }}>
+         <Typography color="primary" variant="h1" textAlign="center"> Yuri Oliveira </Typography>
+         <Typography color="primary" variant="h2" textAlign="center"> I'm a Fullstack Software Engineer! </Typography>
+         <Grid container display="flex" justifyContent="center">
+
+        <Grid size={{ xs: 12, md: 4 }} display="flex" justifyContent="center">
+
+         <Button>
+          <EmailIcon />
+           Contact Me
+          </Button>
+
+        </Grid>
+
+        <Grid size={{ xs: 12, md: 4 }} display="flex" justifyContent="center">
+         <Button>
+          <DownloadIcon />
+           Download CV
+          </Button>
+
+        </Grid>
+
+          </Grid>
+        </Grid>
+      </Grid>
+
+      </Container>
     </StyledHero>
+  );
+};
 
-    </>
-  )
-}
-
-export default Hero
+export default Hero;
---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#0000FF",
+    },
+    secondary: {
+      main: "#F8F8FF",
+    },
+  },
+});
+
+export default theme;
